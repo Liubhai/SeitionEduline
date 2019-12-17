d1e664e2ee300b85501e4e1eed43ed981549ace6
刘邦海 <754230638@qq.com>
2019-12-17 15:38:23 +0800

    code:腾鲤马更换三方信息

--- ChuYouYun/Service/SYG.h
+++ ChuYouYun/Service/SYG.h
@@ -204,11 +204,11 @@ alpha:1.0]
 #define MACRO_UI_SAFEAREA [YunKeTang_Api_Tool safeAreaWithIPhoneX]
 
 // 微信分享
-#define WXAppId @"wxbbb961a0b0bf577a"
-#define WXAppSecret @"7ea0101aeabd53bc32859370cde278cc"
+#define WXAppId @"wx10ae0bb201a2c632"
+#define WXAppSecret @"17740bf81da0bf838656426f2c6dd292"
 // QQ分享
-#define QQAppId @"101400042"
-#define QQAppSecret @"a85c2fcd67839693d5c0bf13bec84779"
+#define QQAppId @"101735846"
+#define QQAppSecret @"d36d09a6a6451c91d341c00a586ac5f6"
 // 新浪分享
 #define SinaAppId @"3997129963"
 #define SinaAppSecret @"da07bcf6c9f30281e684f8abfd0b4fca"
